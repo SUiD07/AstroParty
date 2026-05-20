4085fc5bbbb32866ae88f3b7721252bbbacc1cae
Sirada U <nan.uthaiwat@gmail.com>
2026-05-20 23:23:50 +0700

    fix: change iframe allowfullscreen prop to allowFullScreen in ViewerDashboard

--- components/mine/ViewerDashboard.tsx
+++ components/mine/ViewerDashboard.tsx
@@ -657,7 +657,7 @@ function QuestionModal({
             // loading="lazy"
             // style="position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;"
             src="https://www.canva.com/design/DAHJcsfAYkA/uw5444kV3WhaQGvVdCxuMQ/view?embed"
-            allowfullscreen="allowfullscreen"
+            allowFullScreen
             allow="fullscreen"
           ></iframe>
         </div>
